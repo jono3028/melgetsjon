a721ccc632d429abaf05710313f206e4c58b67c2
jono3028 <jonowen@hotmail.com>
2019-01-02 20:15:40 -0800

    Added css-loader and style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
 	module: {
 		rules: [
 			{ test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+			{ test: /\.css$/, loader: "style-loader!css-loader" },
 			{ enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
 		]
 	},
